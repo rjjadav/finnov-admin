576ed7778ce99eb0f9d10af38623b904a4c8a762
Rahul Jadav <rj.jadav22@gmail.com>
2017-03-25 13:11:49 +0530

    port issue

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 3001 , function(){
+app.listen(process.env.PORT || 3000 , function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
